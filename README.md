b10b87695dc053a8048b49183347c4a0b21542a1
mohsen <amirimohsen72@gmail.com>
2025-07-05 12:48:01 +0330

    #4 send_message in massega_handler

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ docoument for read in docs folder.
 
  auth:
  telebot.TeleBot('API_TOKEN_String')
+
+ message handler(get) and send messega (send):
+ @bot.message_handler()
+def send_wellcome(message):
+    if message.text == 'start' :
+        bot.send_message(message.chat.id,'خوش آمدید')
+
--- core/main.py
+++ core/main.py
@@ -10,7 +10,13 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler()
 def send_wellcome(message):
     if message.text == 'start' :
+        bot.send_message(message.chat.id,'خوش آمدید')
         print(1111)
+    elif message.text.startswith('سلام') or message.text.startswith('hi') :
+        bot.send_message(message.chat.id,'علیک سلام')
+        print(1111)
+    else :
+        bot.send_message(message.chat.id, 'پیام دریافت شد . بعد از بررسی پاسخ خواهیم داد')
 
 
 bot.infinity_polling()
